acde6de79bdbad3c521e539be7f53cf9a5c30c41
Christopher-Lord <lordc42@gmail.com>
2025-09-25 12:47:14 +0200

    Update document event listner with retrieveTheme function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,11 @@
 
 // Import all events and main task function
 import "./events.js";
+import { retrieveTheme } from "./storage.js";
 import { renderTasks } from "./tasks.js";
 
 // Only runs renderTasks once the DOM is loaded
-document.addEventListener("DOMContentLoaded", renderTasks);
+document.addEventListener("DOMContentLoaded", function () {
+  retrieveTheme();
+  renderTasks();
+});
